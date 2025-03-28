7814694555c9475b583266500c35246628aaeb5b
Naeem Prasla <mhkprasla@gmail.com>
2025-03-28 08:01:11 +0500

    Api Handling Feature

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -2,6 +2,7 @@ import { Component } from '../../core/Component';
 import { Header } from '../common/Header';
 import { Navbar } from '../common/Navbar';
 import { Footer } from '../common/Footer';
+import { UserList } from '../common/UserList';
 
 export class AboutPage extends Component {
   render() {
@@ -11,6 +12,7 @@ export class AboutPage extends Component {
       <main class="container">
         <h2>About Us</h2>
         <p>Learn about our company history and team.</p>
+        ${new UserList().render()}
       </main>
       ${new Footer().render()}
     `;
